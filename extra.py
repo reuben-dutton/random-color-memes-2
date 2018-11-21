d75a8f1f46d58e51a8e77bf4bc20ae7ff4381b9b
Reuben <Earthbaker@users.noreply.github.com>
2018-11-21 19:36:19 +1000

    Fixed yet again some more stuff

--- extra.py
+++ extra.py
@@ -1,4 +1,5 @@
 from PIL import Image, ImageDraw, ImageFilter
+import math
 
 
 def text_center(text, font, fill, bounds, image):
@@ -33,52 +34,6 @@ def text_center(text, font, fill, bounds, image):
                           spacing=10,
                           align="center")
     return image
-
-
-def rounded_rectangle(draw, xy, corner_radius, fill=None, outline=None):
-    upper_left_point = xy[0]
-    bottom_right_point = xy[1]
-    draw.rectangle(
-        [
-            (upper_left_point[0], upper_left_point[1] + corner_radius),
-            (bottom_right_point[0], bottom_right_point[1] - corner_radius)
-        ],
-        fill=fill,
-        outline=outline
-    )
-    draw.rectangle(
-        [
-            (upper_left_point[0] + corner_radius, upper_left_point[1]),
-            (bottom_right_point[0] - corner_radius, bottom_right_point[1])
-        ],
-        fill=fill,
-        outline=outline
-    )
-    draw.pieslice([upper_left_point, (upper_left_point[0] + corner_radius * 2, upper_left_point[1] + corner_radius * 2)],
-        180,
-        270,
-        fill=fill,
-        outline=outline
-    )
-    draw.pieslice([(bottom_right_point[0] - corner_radius * 2, bottom_right_point[1] - corner_radius * 2), bottom_right_point],
-        0,
-        90,
-        fill=fill,
-        outline=outline
-    )
-    draw.pieslice([(upper_left_point[0], bottom_right_point[1] - corner_radius * 2), (upper_left_point[0] + corner_radius * 2, bottom_right_point[1])],
-        90,
-        180,
-        fill=fill,
-        outline=outline
-    )
-    draw.pieslice([(bottom_right_point[0] - corner_radius * 2, upper_left_point[1]), (bottom_right_point[0], upper_left_point[1] + corner_radius * 2)],
-        270,
-        360,
-        fill=fill,
-        outline=outline
-    )
-
 
 def convert_hsv(r, g, b):
     '''
